da6557612d864045217377926242f9a802cef29c
Volya23 <v.o.kryzhanivska@gmail.com>
2023-10-21 01:11:48 +0300

    counter

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
     }
 }
 
-const Component = React.createElement(Head, {text: 'React'});
+const Component = React.createElement(Head, {text: 'React'}, 'Text');
 const Component2 = React.createElement(Head, {text: 'JavaScript'});
 const parent = React.createElement('section', {}, Component, Component2);
 
@@ -18,7 +18,7 @@ const root = document.querySelector('#root');
 
 ReactDOM.render(parent, root);*/
 
-class Nik extends React.Component {
+/*class Nik extends React.Component {
     render() {
         const h1 = React.createElement ('h1', {}, `Привіт, ${this.props.text}`);
 
@@ -28,8 +28,47 @@ class Nik extends React.Component {
 
 const Component3 = React.createElement(Nik, {text: 'John'});
 
-const oot = document.querySelector('#root');
+const root = document.querySelector('#root');
 
-ReactDOM.render(Component3, root);
+ReactDOM.render(Component3 , root);*/
 
 
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count:0
+        }
+    }
+
+    increment () {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+
+    decrement () {
+        if(this.state.count > 0) {
+            this.setState({
+            count: this.state.count - 1
+        })
+        } else if (this.state.count === 0) {
+            this.state({
+                count: 0
+            })
+        }
+    }
+
+    render() {
+        const h2 = React.createElement('h2', {}, this.state.count);
+        const button = React.createElement('button', {onClick: () => {this.increment()}}, '+');
+        const button2 = React.createElement('button', {onClick: () => {this.decrement()}}, '-')
+
+        return React.createElement(React.Fragment, {}, h2, button, button2);
+    }
+} 
+
+const component4 = React.createElement(Counter);
+
+const root = document.querySelector('#root');
+ReactDOM.render(component4, root);
